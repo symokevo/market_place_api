c37c68131a48b27bd2a27dfe972b32d8996ad2b1
Simon Rabuogi <symokevo@gmail.com>
2023-05-22 03:16:53 +0300

    Set the route constraints for the API

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   # Application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  #API definition 
+  namespace :api, defaults: { format: :json } do
+    
+    #resourcers listed here
+  end
 
   # Defines the root path route ("/")
   # root "articles#index"
